f1d7f4953520e3abc808c1c07c518bd89fb82321
hejiangyuan <234563@qq.com>
2017-09-12 09:28:46 +0800

    暂存

--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,24 +1,7 @@
 <template>
-    <div class="table-component">
-        <div v-if="showFilter && filterableColumnExists" class="table-component__filter">
-            <input
-                    :class="fullFilterInputClass"
-                    type="text"
-                    v-model="filter"
-                    :placeholder="filterPlaceholder"
-            >
-            <a
-                    v-if="filter"
-                    @click="filter = ''"
-                    class="table-component__filter__clear"
-            >×</a>
-        </div>
-
-        <div class="table-component__table-wrapper">
+    <div class="row">
+        <div class="table-container">
             <table :class="fullTableClass">
-                <caption v-if="showCaption" class="table-component__table__caption" role="alert" aria-live="polite">
-                    {{ ariaCaption }}
-                </caption>
                 <thead>
                 <tr>
                     <table-column-header
@@ -41,40 +24,27 @@
             </table>
         </div>
 
-        <div v-if="displayedRows.length === 0" class="table-component__message">
-            {{ filterNoResults }}
-        </div>
-
-
-        <div style="display:none;">
-            <slot></slot>
-        </div>
-
-        <pagination v-if="pagination" :pagination="pagination" @pageChange="pageChange"></pagination>
     </div>
 </template>
 
 <script>
     import Column from '../classes/Column';
-    import expiringStorage from '../expiring-storage';
     import Row from '../classes/Row';
     import TableColumnHeader from './TableColumnHeader';
     import TableRow from './TableRow';
     import settings from '../settings';
     import isArray from 'lodash/isArray';
     import pick from 'lodash/pick';
-    import Pagination from './Pagination';
     import { classList } from '../helpers';
 
     export default {
         components: {
             TableColumnHeader,
             TableRow,
-            Pagination,
         },
 
         props: {
-            data: {default: () => [], type: [Array, Function]},
+            data: { default: () => [], type: [Array, Function] },
 
             showFilter: { default: true },
             showCaption: { default: true },
@@ -104,113 +74,25 @@
             localSettings: {},
         }),
 
-        created() {
-            this.sort.fieldName = this.sortBy;
-            this.sort.order = this.sortOrder;
-
-            this.restoreState();
-        },
-
         async mounted() {
             this.mapColumns();
 
             await this.mapDataToRows();
         },
 
-        watch: {
-            filter() {
-                if (!this.usesLocalData) {
-                    this.mapDataToRows();
-                }
-
-                this.saveState();
-            },
-
-            data() {
-                if (this.usesLocalData) {
-                    this.mapDataToRows();
-                }
-            },
-        },
-
         computed: {
             fullTableClass() {
-                return classList('table-component__table', this.tableClass);
+                return classList('', this.tableClass);
             },
-
-            fullFilterInputClass() {
-                return classList('table-component__filter__field', this.filterInputClass);
-            },
-
-            ariaCaption() {
-                if (this.sort.fieldName === '') {
-                    return 'Table not sorted';
-                }
-
-                return `Table sorted by ${this.sort.fieldName} ` +
-                    (this.sort.order === 'asc' ? '(ascending)' : '(descending)');
-            },
-
             usesLocalData() {
                 return isArray(this.data);
             },
-
             displayedRows() {
-                if (!this.usesLocalData) {
-                    return this.sortedRows;
-                }
-
-                if (!this.showFilter) {
-                    return this.sortedRows;
-                }
-
-                if (!this.columns.filter(column => column.isFilterable()).length) {
-                    return this.sortedRows;
-                }
-
-                return this.sortedRows.filter(row => row.passesFilter(this.filter));
+                return this.rows;
             },
 
-            sortedRows() {
-                if (!this.usesLocalData) {
-                    return this.rows;
-                }
-
-                if (this.sort.fieldName === '') {
-                    return this.rows;
-                }
-
-                if (this.columns.length === 0) {
-                    return this.rows;
-                }
-
-                const sortColumn = this.getColumn(this.sort.fieldName);
-
-                if (!sortColumn) {
-                    return this.rows;
-                }
-
-                return this.rows.sort(sortColumn.getSortPredicate(this.sort.order, this.columns));
-            },
-
-            filterableColumnExists() {
-                return this.columns.filter(c => c.isFilterable()).length > 0;
-            },
-
-            storageKey() {
-                return this.cacheKey
-                    ? `vue-table-component.${this.cacheKey}`
-                    : `vue-table-component.${window.location.host}${window.location.pathname}${this.cacheKey}`;
-            },
         },
-
         methods: {
-            async pageChange(page) {
-                this.pagination.currentPage = page;
-
-                await this.mapDataToRows();
-            },
-
             async mapDataToRows() {
                 const data = this.usesLocalData
                     ? this.prepareLocalData()
@@ -225,7 +107,6 @@
                     })
                     .map(rowData => new Row(rowData, this.columns));
             },
-
             mapColumns() {
                 const columnComponents = this.$slots.default.filter(
                     column => column.componentInstance
@@ -237,70 +118,20 @@
 
                 columnComponents.forEach(column => {
                     Object.keys(this.$options.props).forEach(
-                         prop => this.$watch(prop, () => this.mapColumns())
+                        prop => this.$watch(prop, () => this.mapColumns())
                     );
                 });
-            },
 
+            },
             prepareLocalData() {
                 this.pagination = null;
 
                 return this.data;
             },
-
             async fetchServerData() {
-                const page = this.pagination && this.pagination.currentPage || 1;
-
-                const response = await this.data({
-                    filter: this.filter,
-                    sort: this.sort,
-                    page: page,
-                });
-
-                this.pagination = response.pagination;
-
-                return response.data;
-            },
-
-            async refresh() {
-                await this.mapDataToRows();
-            },
-
-            changeSorting(column) {
-                if (this.sort.fieldName !== column.show) {
-                    this.sort.fieldName = column.show;
-                    this.sort.order = 'asc';
-                } else {
-                    this.sort.order = (this.sort.order === 'asc' ? 'desc' : 'asc');
-                }
-
-                if (!this.usesLocalData) {
-                    this.mapDataToRows();
-                }
-
-                this.saveState();
-            },
-
-            getColumn(columnName) {
-                return this.columns.find(column => column.show === columnName);
-            },
-
-            saveState() {
-                expiringStorage.set(this.storageKey, pick(this.$data, ['filter', 'sort']), this.cacheLifetime);
-            },
-
-            restoreState() {
-                const previousState = expiringStorage.get(this.storageKey);
-
-                if (previousState === null) {
-                    return;
-                }
-
-                this.sort = previousState.sort;
-                this.filter = previousState.filter;
-
-                this.saveState();
+                return null;
             },
         },
+
     };
 </script>
